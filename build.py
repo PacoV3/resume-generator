0393100d0033521bea0c33df68b291d5f37656f8
Francisco Ventura <44516996+PacoV3@users.noreply.github.com>
2023-09-05 06:09:44 +0000

    Full implementation

--- build.py
+++ build.py
@@ -1,15 +1,3 @@
-"""
-Simple HTML and PDF resume generator from structurized YAML files.
-
-Usage:
-    build.py [-o=<DIR>] [-f=<FORMAT>] [-t=<THEME>] <resume_file>
-
-Options:
-    -o=<DIR>, --output_dir=<DIR>     Output directory for the build files. [default: build].
-    -f=<FORMAT>, --format=<FORMAT>   Format of the build [default: html].
-    -t=<NAME>, --theme=<NAME>        Name of the theme to use.
-"""
-
 import os
 from typing import TypedDict
 
@@ -47,11 +35,12 @@ def copy_static_data(theme_dir, output_dir) -> None:
     shutil.copytree(
         theme_dir,
         output_dir,
-        ignore=lambda src, names: [name for name in names if name.endswith(".jinja2")],
+        ignore=lambda src, names: [
+            name for name in names if name.endswith(".jinja2")],
     )
 
 
-def clean(output_dir: str) -> None:
+def clean(output_dir: str = "build") -> None:
     """
     Remove the output directory.
     """
@@ -59,12 +48,13 @@ def clean(output_dir: str) -> None:
 
 
 # theme -> simple or compact
-def make_html(yaml_data: TypedDict, theme: str, output_dir: str):
+def make_html(yaml_data: TypedDict, theme: str, output_dir: str = "build"):
     """
     Build the final directory, rendering all templates and copying source files
     """
     yaml_data["h"] = helpers
     yaml_data["labels"] = None
+
     theme_location = os.path.join("themes", theme)
     jinja2_template = os.path.join(theme_location, "index.jinja2")
     html_template = os.path.join(output_dir, "index.html")
@@ -77,27 +67,25 @@ def make_html(yaml_data: TypedDict, theme: str, output_dir: str):
         file.write(html)
 
 
-def make_pdf(file_name: str, theme: str, output_dir: str, pdf_dir: str = "./pdf"):
+def make_pdf(file_name: str, theme: str, pdf_dir: str = "pdf", output_dir: str = "build"):
     """
     Generate PDF file out of generated 'index.html' page.
     """
-    output_file = os.path.join(pdf_dir, file_name.replace(".yaml", ".pdf"))
+    output_file = os.path.join(pdf_dir, file_name)
     input_file = os.path.join(output_dir, "index.html")
     theme_location = os.path.join("themes", theme)
     html = HTML(input_file, base_url=theme_location)
     html.write_pdf(output_file)
 
 
-def make_resume_from_yaml(file_name: str, theme: str, output_dir: str):
+def make_resume_from_yaml(file_name: str, theme: str, yaml_dir: str = "resumes",
+                          pdf_dir: str = "pdf", output_dir: str = "build"):
     """
     Both HTML and PDF for a single file.
     """
-    resume_data = read_yaml(file_name)
+    resume_data = read_yaml(os.path.join(yaml_dir, f"{file_name}.yaml"))
     make_html(resume_data, theme, output_dir)
-    # make_pdf(
-    #     file_name,
-    #     theme,
-    # )
+    make_pdf(f"{file_name}.pdf", theme, pdf_dir, output_dir)
 
 
 def main():
@@ -106,15 +94,17 @@ def main():
     and run appropriate build like 'html' and 'pdf'.
     """
     # read resume data and config with some defaults
-    output_dir = "./build"
-    theme = "simple"
-    yaml_file_name = "./resumes/hanula.yaml"
+    yaml_dir = "resumes"
+    pdf_dir = "pdf"
+    output_dir = "build"
+    theme = "compact"
+    # file_name = "jmbeach"
+    # file_name = "hanula"
 
-    # build based on the given format
-    # cmds = {'html': make_html, 'pdf': make_pdf}
-    # return cmds[output_format](config, resume_data)
+    files = ["jmbeach", "hanula"]
 
-    make_resume_from_yaml(yaml_file_name, theme, output_dir)
+    for file in files:
+        make_resume_from_yaml(file, theme, yaml_dir, pdf_dir, output_dir)
 
 
 if __name__ == "__main__":
